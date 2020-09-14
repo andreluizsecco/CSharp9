8fe4e6e0526ce786525bcac77540242704fb5804
André Secco <andreluizsecco@yahoo.com.br>
2020-09-14 08:19:06 -0300

    Initial commit

---
+++ NewExpressions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NewExpressions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Formas convêncionais
+            Carro carro = new Carro("Honda", "WR-V", "AAA-1234", 79921);
+            var carro1 = new Carro("Honda", "WR-V", "AAA-1234", 79921);
+
+            // Nova possibilidade
+            Carro carro2 = new ("Honda", "WR-V", "AAA-1234", 79921);
+
+            Console.WriteLine("Carros:");
+            Console.WriteLine(carro.ToString());
+            Console.WriteLine(carro1.ToString());
+            Console.WriteLine(carro2.ToString());
+        }
+    }
+
+    public class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public decimal Valor { get; set; }
+
+        public Carro(string marca, string modelo, string placa, decimal valor)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;
+            Valor = valor;
+        }
+
+        public override string ToString() =>
+            $"Marca: {Marca} | Modelo: {Modelo} | Placa: {Placa} | Valor: {Valor:C}";
+    }
+}
---
+++ TargetTyping/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+
+namespace TargetTyping
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Target Typed - New Expressions
+            Point point = new(3, 5);
+
+            Console.WriteLine($"New Expressions: X => {point.X} | Y => {point.Y}");
+
+            // Target Typing Conditional Operators
+
+            // Nullable Value Types
+            var resultado = 99;
+            int? contador = resultado == 1 ? 0 : null;
+
+            Console.WriteLine($"Nullable Value Types: {contador}");
+
+            
+            //Shared Base Types
+            Gato gato = null;
+            Cachorro cachorro = new();
+
+            // OBS: Nesse preview (preview 8) ainda está acusando erro de compilação
+            Animal animal = gato ?? cachorro;
+        }
+    }
+
+    public class Animal { }
+
+    public class Gato : Animal { }
+
+    public class Cachorro : Animal { }
+}
